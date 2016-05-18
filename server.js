a473c447b4fd97efdc507f7292fd36c8df61815a
jrisk <joeyrsk@gmail.com>
2016-05-18 19:44:30 -0400

    move dev server to node/express from lite-server

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const http = require('http');
 
+var express = require('express');
+
+var app = express();
+
+//var buf = new Buffer(10);
+
 const hostname = '127.0.0.1';
 
 const port = 3002;
 
-const server = http.createServer( (req,res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('hey globe');
-});
+console.log(app);
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
 	console.log(`server running on http://${hostname}:${port}/`);
