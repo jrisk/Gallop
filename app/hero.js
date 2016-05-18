ace015cc9206b67c9e7f5fbe8e185a1c531e44d1
jrisk <joeyrsk@gmail.com>
2016-05-18 18:19:56 -0400

    removed js & js.map files

--- app/hero.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.HEROES = [
-    { name: 'Mr. IQ' },
-    { name: 'Magneta' },
-    { name: 'Bombasto' }
-];
-//# sourceMappingURL=hero.js.map
--- app/messages.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.messages = [
-    { msg: 'Welcome to Gallop! A chat/video site done in Angular 2', sender: 'username1234', time: '12pm' }
-];
-//# sourceMappingURL=messages.js.map
